c0cb5f62a43f23818412ff6e88b2b49111201522
Xinyuan <xinyuanzheng4@gmail.com>
2025-06-03 14:19:19 -0400

    initial commit

---
+++ app/components/About.jsx
@@ -0,0 +1,114 @@
+import { assets, infoList, toolsData } from "@/assets/assets";
+import Image from "next/image";
+import React from "react";
+import { motion } from "motion/react";
+const About = ({ isDarkMode }) => {
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      transition={{ duration: 1 }}
+      className="w-full px-[12%] py-10 scroll-mt-20"
+      id="about"
+    >
+      <motion.h4
+        initial={{ opacity: 0, y: -20 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5, delay: 0.3 }}
+        className="text-center mb-2 text-lg font-Ovo"
+      >
+        Introduction
+      </motion.h4>
+      <motion.h2
+        initial={{ opacity: 0, y: -20 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5, delay: 0.5 }}
+        className="text-center text-5xl font-Ovo"
+      >
+        About Me
+      </motion.h2>
+      <motion.div
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ duration: 0.8 }}
+        className="flex w-full flex-col lg:flex-row items-center gap-20 my-20"
+      >
+        <motion.div
+          initial={{ opacity: 0, scale: 0.9 }}
+          whileInView={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.6 }}
+          className="w-64 sm:w-80 rounded-3xl max-w-none"
+        >
+          <Image
+            src={assets.user_image}
+            alt="user"
+            className="w-full rounded-3xl"
+          />
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0 }}
+          whileInView={{ opacity: 1 }}
+          transition={{ duration: 0.6, delay: 0.8 }}
+          className="flex-1"
+        >
+          <p className="mb-10 max-w-2xl">
+            I'm a software engineer who loves turning complex problems into
+            clean, efficient code using React, JavaScript, and Node.js.
+          </p>
+          <motion.ul
+            initial={{ opacity: 0 }}
+            whileInView={{ opacity: 1 }}
+            transition={{ duration: 0.8, delay: 1 }}
+            className="grid grid-cols-1 sm:grid-cols-3 gap-6 max-w-2xl"
+          >
+            {infoList.map((info, index) => (
+              <motion.li
+                whileHover={{ scale: 1.05 }}
+                key={index}
+                className="border-[0.5px] border-gray-400 rounded-xl p-6 cursor-pointer hover:bg-rose-200 hover:-translate-y-1 duration-500 hover:shadow-black dark:border-white dark:hover:shadow-white"
+              >
+                <Image
+                  src={isDarkMode ? info.iconDark : info.icon}
+                  alt={info.title}
+                  className="w-7 mt-3"
+                />
+                <h3 className="my-4 font-semibold text-gray-700 dark:text-white">
+                  {info.title}
+                </h3>
+                <p className="text-gray-600 text-sm dark:text-white/80">
+                  {info.description}
+                </p>
+              </motion.li>
+            ))}
+          </motion.ul>
+          <motion.h4
+            initial={{ opacity: 0, y: 20 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5, delay: 1.3 }}
+            className="my-6 text-gray-700 dark:text-white/80"
+          >
+            Tools I use
+          </motion.h4>
+          <motion.ul
+            initial={{ opacity: 0 }}
+            whileInView={{ opacity: 1 }}
+            transition={{ duration: 0.5, delay: 1.5 }}
+            className="flex items-center gap-3 sm:gap-5"
+          >
+            {toolsData.map((tool, index) => (
+              <motion.li
+                whileHover={{ scale: 1.1 }}
+                key={index}
+                className="flex items-center justify-center w-12 sm:w-14 border border-gray-400 rounded-lg cursor-pointer hover:-translate-y-1 duration-500 aspect-square"
+              >
+                <Image src={tool} alt="" className="w-5 sm:w-7" />
+              </motion.li>
+            ))}
+          </motion.ul>
+        </motion.div>
+      </motion.div>
+    </motion.div>
+  );
+};
+
+export default About;
---
+++ app/components/Header.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+import Image from "next/image";
+import { assets } from "@/assets/assets";
+import { motion } from "motion/react";
+
+const Header = () => {
+  return (
+    <div className="w-11/12 max-w-3xl text-center mx-auto h-screen flex flex-col items-center justify-center gap-4">
+      <motion.h3
+        initial={{ y: -20, opacity: 0 }}
+        whileInView={{ y: 0, opacity: 1 }}
+        transition={{ duration: 0.6, delay: 0.3 }}
+        className="flex items-end gap-2 text-xl md:text-2xl mb-3 font-Ovo"
+      >
+        Hi! I'm Xinyuan Zheng
+        <Image src={assets.hand_icon} alt="" className="w-6" />
+      </motion.h3>
+      <motion.h1
+        initial={{ y: -30, opacity: 0 }}
+        whileInView={{ y: 0, opacity: 1 }}
+        transition={{ duration: 0.8, delay: 0.5 }}
+        className="text-3xl sm:text-6xl lg:text-[66px] font-Ovo"
+      >
+        Software Engineer based in Quincy.
+      </motion.h1>
+      <motion.p
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ duration: 0.6, delay: 0.7 }}
+        className="max-w-2xl mx-auto font-Ovo"
+      >
+        I am a Software Engineer from Quincy, Massachusetts with 3 years of
+        experience in JPMorgan Chase and BeaconFire.
+      </motion.p>
+      <div className="flex flex-col sm:flex-row items-center gap-4 mt-4">
+        <motion.a
+          initial={{ y: 30, opacity: 0 }}
+          whileInView={{ y: 0, opacity: 1 }}
+          transition={{ duration: 0.6, delay: 1 }}
+          href="#contact"
+          className="px-10 py-3 border border-white rounded-full bg-black text-white flex items-center gap-2 dark:bg-transparent"
+        >
+          Contact me{" "}
+          <Image src={assets.right_arrow_white} alt="" className="w-4" />
+        </motion.a>
+        <motion.a
+          initial={{ y: 30, opacity: 0 }}
+          whileInView={{ y: 0, opacity: 1 }}
+          transition={{ duration: 0.6, delay: 1.2 }}
+          href="/Xinyuan_Zheng_Resume.docx"
+          download
+          className="px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2 bg-white dark:text-black"
+        >
+          My Resume <Image src={assets.download_icon} alt="" className="w-4" />
+        </motion.a>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
