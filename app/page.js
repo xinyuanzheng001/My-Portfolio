2a82f586275318654256b37164719dbe41569c75
Xinyuan <xinyuanzheng4@gmail.com>
2025-06-12 22:42:27 -0400

    remove visit notification

--- app/page.js
+++ app/page.js
@@ -18,17 +18,17 @@ export default function Home() {
     }
   }, []);
 
-  useEffect(() => {
-    const formData = new FormData();
-    formData.append("access_key", "c5f24aba-7be5-4760-bbd0-75b32bf00d50");
-    formData.append("email", "xinyuanzheng4@gmail.com");
-    formData.append("message", "People visited");
-    formData.append("name", "Visitor");
-    fetch("https://api.web3forms.com/submit", {
-      method: "POST",
-      body: formData,
-    });
-  }, []);
+  // useEffect(() => {
+  //   const formData = new FormData();
+  //   formData.append("access_key", "c5f24aba-7be5-4760-bbd0-75b32bf00d50");
+  //   formData.append("email", "xinyuanzheng4@gmail.com");
+  //   formData.append("message", "People visited");
+  //   formData.append("name", "Visitor");
+  //   fetch("https://api.web3forms.com/submit", {
+  //     method: "POST",
+  //     body: formData,
+  //   });
+  // }, []);
 
   useEffect(() => {
     if (isDarkMode) {
